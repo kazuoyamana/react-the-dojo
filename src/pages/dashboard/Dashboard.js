50df934a3d8d79ca9dc302916322c093a9025ce9
Kazuo Yamana <k.yamana@gmail.com>
2022-10-03 10:50:27 +0900

    add project filter function

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,14 +1,39 @@
-import { ProjectList } from "../../components/ProjectList"
-import { useCollection } from "../../hooks/useCollection"
 import "./Dashboard.css"
+import { useState } from "react"
+import { ProjectList } from "../../components/ProjectList"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useCollection } from "../../hooks/useCollection"
+import { ProjectFilter } from "./ProjectFilter"
 
 export const Dashboard = () => {
   const { documents, error } = useCollection("projects")
+  const [currentFilter, setCurrentFilter] = useState("all")
+  const { user } = useAuthContext()
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter)
+    console.log(documents, user)
+  }
+
+  const projects = documents?.filter((doc) => {
+    if (currentFilter === "all") {
+      // 下記のようにdocをそのまま返しても true を返しても同じ
+      return doc
+    } else if (currentFilter === "mine") {
+      return doc.assignedUsersList.map((u) => u.id).includes(user.uid)
+    } else {
+      return doc.category === currentFilter // これも結局、式の結果をtrue/falseで返してるだけ
+    }
+  })
+
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {documents && (
+        <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />
+      )}
+      {projects && <ProjectList projects={projects} />}
     </div>
   )
 }
